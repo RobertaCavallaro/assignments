17fb3f73b7173996c1af96df417686f6ef23d813
RobertaCt <92271727+RobertaCavallaro@users.noreply.github.com>
2026-09-14 12:57:18 -0700

    Update assignment04.py

--- CISC179/W4/assignment04.py
+++ CISC179/W4/assignment04.py
@@ -1,1 +1,23 @@
+##1a) Take five inputs from an user and save it in a tuple called my_tuple
+dato1 = input("what's your name: ")
+dato2 = input("how old are you: ")
+dato3 = input("how are you: ")
+dato4 = input("where are you from: ")
+dato5 = input("do you like python: ")
+
+my_tuple = (dato1, dato2, dato3, dato4, dato5)
+
+print("Account is: ")
+print(my_tuple)
+
+##tuples are unchangeable!!
+
+######Count the repeated integers and print the result on the console.
+my_tuple = (1,2,3,4,3,2,1,2,3,5,4,3,2,1)
+print("How many 1:", my_tuple.count(1))
+print("How many 2:", my_tuple.count(2))
+print("How many 3:", my_tuple.count(3))
+print("How many 4:", my_tuple.count(4))
+print("How many 5:", my_tuple.count(5))
+
 
